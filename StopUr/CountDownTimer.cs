340168ae58656e9c526603c5ed93a7c9d2894f75
marc8847 <marc8847@ZBC-E-AH-9287>
2022-01-03 15:55:30 +0100

    Add project files.

---
+++ StopUr/CountDownTimer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StopUr
+{
+    public class CountDownTimer
+    {
+        private int countdownSeconds;
+        private Thread thread;
+        private bool run;
+
+        public event EventHandler CountDownChange;
+        public int CountdownSeconds { get { return countdownSeconds;} }
+        public CountDownTimer(int hour, int min, int sec)
+        {
+            this.countdownSeconds = ConvertToSec(hour, min, sec);
+        }
+        /// <summary>
+        /// Converts to seconds for easier Calculations
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="m"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private int ConvertToSec(int h, int m, int s)
+        {
+            int seconds = 0;
+            seconds += (h * 3600);
+            seconds += (m * 60);
+            seconds += s;
+            return seconds;
+        }
+
+        /// <summary>
+        /// Converts to a string format to later use in override to string
+        /// </summary>
+        /// <returns></returns>
+        private string ConvertToStringFormat()
+        {
+            int cs = countdownSeconds;
+            int h, m, s;
+            h = cs / 3600;
+            m =  (cs / 60) % 60;
+            s = cs % 60;
+            return string.Format("{0} : {1} : {2}", h, m, s);
+        }
+
+        /// <summary>
+        /// Creates a thread with countdown method then starts it
+        /// </summary>
+        public void StartTimer()
+        {
+            this.thread = new Thread(Countdown);
+            this.thread.Start();
+            run = true;
+        }
+
+        /// <summary>
+        /// Sets run to false to stop timer
+        /// </summary>
+        public void StopTimer()
+        {
+            run = false;
+        }
+
+        /// <summary>
+        /// Countdown if countdownSeconds is over zero and run is true, then post through event, and waits a sec
+        /// </summary>
+        public void Countdown()
+        {
+            while (this.countdownSeconds > 0 && this.run == true)
+            {
+                this.countdownSeconds--;
+                OnCountChange(EventArgs.Empty);
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// <summary>
+        /// OnCountChange Invokes on event
+        /// </summary>
+        /// <param name="e"></param>
+        private void OnCountChange(EventArgs e)
+        {
+            this.CountDownChange?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// overrides tostring
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return ConvertToStringFormat();
+        }
+    }
+}
---
+++ StopUr/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StopUr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public CountDownTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void kage(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() => Showtimer.Content = timer.ToString());
+        }
+        private void createTimer_Click(object sender, RoutedEventArgs e)
+        {
+            timer = new CountDownTimer((int)hours.Value, (int)minutes.Value, (int)seconds.Value);
+            hours.Value = 0;
+            minutes.Value = 0;
+            seconds.Value 0;
+            timer.CountDownChange += kage;
+        }
+        private void startTimer_Click(object sender, RoutedEventArgs e)
+        {
+            timer.StartTimer();
+        }
+        private void stopTimer_Click(object sender, RoutedEventArgs e)
+        {
+            timer.StopTimer();
+        }
+
+        
+    }
+}
